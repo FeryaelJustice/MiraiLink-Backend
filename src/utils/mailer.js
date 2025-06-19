d1a2584e799da4083f92e6d51636d3fc9e51f4ad
FeryaelJustice <nano9gs@hotmail.es>
2025-06-20 00:06:43 +0200

    env mailer fix finall

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -4,7 +4,7 @@ import nodemailer from 'nodemailer';
 const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
-    secure: true,
+    secure: process.env.EMAIL_SECURE, // true for 465, false for other ports
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD
